a2b1022b0f00548ff48e88704fad9f9ea817b2b6
iamjokerz <112417798+iamjokerz@users.noreply.github.com>
2025-07-31 08:24:44 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~0vdkkJhD#OCdYbHdsA3y841Itf9OaqIHq61Krz80FNCEI5Jzsbw8",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2349020721745", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
